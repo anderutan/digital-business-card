3c104b79546341404101982611929892a946acd5
anderu <andrewtan1310@gmail.com>
2024-01-26 12:00:16 +0800

    Add About and Interests

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -1,10 +1,14 @@
 import Info from './Info';
+import About from './About';
+import Interests from './Interests';
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
       <Info />
+      <About />
+      <Interests />
     </div>
   );
 }
---
+++ digital-business-card/src/Interests.jsx
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <div className='interests-container'>
+      <h2 className='interests-title'>Interests</h2>
+      <p className='interest-description'>
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
